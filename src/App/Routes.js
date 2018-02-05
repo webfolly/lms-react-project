2eb3ef6a239ebc47486b428a286a2a78047e98e9
webfolly <zhanghuadong2015@gmail.com>
2018-02-05 17:30:46 +0800

    add bootstrap and react-router-dom dependencies

--- src/App/Routes.js
+++ src/App/Routes.js
@@ -1,1 +1,10 @@
-
+export default () => ((
+    <div>
+        <Route exact path="/" component={CoursesView} />
+        <Route exact path="/courses" component={CoursesView} />
+        <Route exact path="/courses/:id" component={CourseDetailView} />
+        <Route exact path="/students" component={StudentsView} />
+        <Route exact path="/lecturers" component={LecturersView} />
+        <Route exact path="/signin" component={SigninView} />
+    </div>
+    ));
---
+++ src/App/TopNav.js
@@ -0,0 +1,17 @@
+export default function TopNav() {
+    return (
+        <nav>
+            <ul className="nav navbar-nav">
+                <li>
+                    <Link to="/courses">Courses</Link>
+                </li>
+                <li>
+                    <Link to="/students">Students</Link>
+                </li>
+                <li>
+                    <Link to="/lecturers">Lecturers</Link>
+                </li>
+            </ul>
+        </nav>
+    );
+}
