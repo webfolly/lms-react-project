da78f5ec9329cc45dd67d9e2c8f079261e4d5444
jerry huang <camper.jxhuang@gmail.com>
2018-02-10 00:19:51 +1000

    details, cards, students view details , cards, students view :

--- src/Student/StudentCard.js
+++ src/Student/StudentCard.js
@@ -4,16 +4,18 @@ import StudentDetailView from "./StudentDetailView";
 import avatar from '../images/img_avatar2.png';
 
 export default class StudentsView extends Component {
+
+
     render() {
         return (
             <div className='col-sm-3 col-md-3 col-lg-3'>
-                <div className="card" style={{width: '100%'}}>
+                <div className="card" style={{width: '100%', marginTop: '15px'}}>
                     <img src={avatar} alt="Avatar" style={{width: '100%'}}/>
                     <div className="container">
-                        <h4><b>Jane Doe</b></h4>
+                        <h4><b>{this.props.student.name}</b></h4>
                         <div>Interior Designer</div>
                         <p>Students Views</p>
-                        <p><Link to={'/students/detail/2'}>Details</Link></p>
+                        <p><Link to={`/students/detail/${this.props.student.id}`}>Details</Link></p>
                     </div>
                 </div>
             </div>
--- src/Student/StudentsView.js
+++ src/Student/StudentsView.js
@@ -7,14 +7,29 @@ import avatar from '../images/img_avatar2.png';
 export default class StudentsView extends Component {
     constructor(props) {
         super(props);
-
-
+        this.state = {
+            students: [
+                {id: 1, name: 'Mike Nud', gender: 'm'},
+                {id: 2, name: 'Luke Lanny', gender: 'm'},
+                {id: 3, name: 'Joe Danny', gender: 'm'},
+                {id: 4, name: 'John Bon', gender: 'm'},
+                {id: 4, name: 'Peter Latasha', gender: 'm'}
+            ]
+        }
     }
 
+
     render() {
+
         return (
+
             <div className={'container-fluid'}>
-              <StudentCard />
+                {this.state.students.map(function (s) {
+                    return <StudentCard key={s.id} student={s}/>
+                })
+                }
+
+
             </div>
         );
 
