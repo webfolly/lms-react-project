790c96f94ce19f9aa2ed48914b54d215cae9de3d
Miya <miya.wu0930@gmail.com>
2018-02-09 15:00:17 +1000

    add CourseCard, CourseView, css.

--- src/Course/CourseCard.js
+++ src/Course/CourseCard.js
@@ -1,3 +1,23 @@
-/**
- * Created by Weiping on 18/11/17.
- */
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+import CourseDetailView from "./CourseDetailView";
+import avatar from '../images/img_avatar2.png';
+
+export default class CoursessView extends Component {
+    render() {
+        return (
+            <div className='col-sm-3 col-md-3 col-lg-3'>
+                <div className="card" style={{width: '100%'}}>
+                    <img src={avatar} alt="Avatar" style={{width: '100%'}}/>
+                    <div className="container">
+                        <h4><b>Jane Doe</b></h4>
+                        <div>Interior Designer</div>
+                        <p>Courses Views</p>
+                        <p><Link to={'/students/detail/2'}>Details</Link></p>
+                    </div>
+                </div>
+            </div>
+        );
+
+    }
+}
---
+++ src/Course/CoursesView.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+import StudentDetailView from "./StudentDetailView";
+import StudentCard from "./StudentCard";
+import avatar from '../images/img_avatar2.png';
+
+export default class StudentsView extends Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+            <div className={'container-fluid'}>
+              <StudentCard />
+            </div>
+    }
+}
