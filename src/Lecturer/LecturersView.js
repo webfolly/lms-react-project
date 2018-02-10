007c6b4531554ce8164312dec05c7c3a449b69f9
idea-PC\jim <jianghua.hui@uq.net.au>
2018-02-10 14:48:24 +1000

    LecturerView.js updated

---
+++ src/Lecturer/LecturersView.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+import LecturerDetailView from "./LecturerDetailView";
+import LecturerCard from "./LecturerCard";
+
+
+export default class LecturersView extends Component {
+    constructor(props) {
+        super(props);
+
+
+    }
+
+    render() {
+        return (
+            <div className={'container-fluid'}>
+              <LecturerCard />
+            </div>
+        );
+
+    }
+
+
+}
