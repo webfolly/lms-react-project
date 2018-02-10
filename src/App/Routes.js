d17c16dacc50aeab5c5a60bcf259842d1a5ef450
jerry huang <camper.jxhuang@gmail.com>
2018-02-10 14:44:11 +1000

    home + routes

--- src/App/Routes.js
+++ src/App/Routes.js
@@ -2,11 +2,11 @@ import React from 'react';
 import {Route} from 'react-router-dom';
 import StudentsView from '../Student/StudentsView';
 import StudentDetailView from '../Student/StudentDetailView';
-
+import Home from './Home';
 
 export default () => ((
     <div>
-        <Route exact path="/" component={() => <p>Home</p> } />
+        <Route exact path="/" component={Home} />
         <Route exact path="/courses" component={() => <p>Course</p>} />
         <Route exact path="/courses/:id" component={() => <p>courses/:id</p>} />
         <Route exact path="/students" component={StudentsView} />
