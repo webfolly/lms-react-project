aa3135cf5152302189976488c0534ae42b2d80b5
Miya <miya.wu0930@gmail.com>
2018-02-12 19:09:36 +1000

    revised courseDetailView route

--- src/App/Routes.js
+++ src/App/Routes.js
@@ -10,7 +10,7 @@ export default () => ((
     <div>
         <Route exact path="/" component={Home}/>
         <Route exact path="/courses" component={CoursesView}/>
-        <Route exact path="/courses/:id" component={CourseDetailView}/>
+        <Route exact path="/courses/detail/:id" component={CourseDetailView}/>
 
         <Route exact path="/students" component={StudentsView}/>
         <Route exact path="/students/detail/:action/:id" component={StudentDetailView}/>
