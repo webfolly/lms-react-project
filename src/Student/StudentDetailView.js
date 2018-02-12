2865a32354886f9ef9cd7d4f4cc4ba5fcf7c627d
idea-PC\jim <jianghua.hui@uq.net.au>
2018-02-12 19:42:02 +1000

    StudentDetailView.js updated

--- src/Student/StudentDetailView.js
+++ src/Student/StudentDetailView.js
@@ -11,7 +11,7 @@ export default class StudentDetailView extends Component{
         super(props);
 
         this.state = {
-            id : props.match.params.id
+            // id : props.match.params.id
         }
 
     }
