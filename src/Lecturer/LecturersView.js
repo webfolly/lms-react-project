974d3a081797da8f3d2d0d93b29d7235c7baa877
idea-PC\jim <jianghua.hui@uq.net.au>
2018-02-12 20:22:15 +1000

    LecturerView.js updated

--- src/Lecturer/LecturersView.js
+++ src/Lecturer/LecturersView.js
@@ -9,7 +9,7 @@ export default class LecturersView extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            lecturers: []
+            lecturers: lecturer
         };
 
 
