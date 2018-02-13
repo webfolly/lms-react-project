0719406caae5ee64936f7fa938096ff8e970345a
idea-PC\jim <jianghua.hui@uq.net.au>
2018-02-13 20:45:25 +1000

    LecturerDetailView.js, LecturersView.js updated

--- src/Lecturer/LecturerDetailView.js
+++ src/Lecturer/LecturerDetailView.js
@@ -1,8 +1,11 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
-import {lecturer} from '../lecturerdata';
-import DetailForm from './DetailForm'; 
+//import {lecturer} from '../lecturerdata';
+//import DetailForm from './DetailForm'; 
 import avatar from '../images/img_avatar2.png';
+//import lecturer from '../api/lecturer';
+
+import {fetchLecturers,  fetchLecturersByID} from '../api/lecturer';
 
 export default class LecturerDetailView extends Component {
     
@@ -19,31 +22,70 @@ export default class LecturerDetailView extends Component {
         //     email: props.match.params.email
         //  }
         this.state = {
-            isLoading: false,
-            isEditing: false,
-            isSaving: false,
-            error: null,
-            lecturer: lecturer
+            lecturer: []
         };
     }
     componentWillMount() {
-        this.setState({lecturer});
+        fetchLecturers()
+        .then(
+            data => this.setState({lecturers: data}))
+        .catch(function (error) {
+            if(error.response) {
+                console.log("There is an error in the lecturer data", error)
+            }
+        } )    
 
+
+        fetchLecturersByID(this.state.lecturers)
+        .then(
+            data1 => this.setState({lecturers: data1.id}))
+        .catch(function (error) {
+            console.log("There is an error in the lecuturer in the lecturer id data", error)
+        })
     }
 
-    renderDetailView() {
-        const lecturer = this.state.lecturer;
-        // console.log(lecturer)
-        return (
-            <DetailForm lecturer={lecturer}/>
-            );        
-    }
+    // handleLecturerDetailViewData(data) {
+    //     let lecturerdata  = [];
+    //     console.log('got lecturerdata  from api:', data);
+    //     for(let i=0; i<5; i++){
+    //         lecturerdata[i] = {name: data[i].name, occupation: data[i].occupation, school: data[i].school, faculty: data[i].faculty, phone: data[i].phone, email: data[i].email}
+    //     }
+    //     this.setState({lecturers})
+        
+
+    // }
+
+    // renderDetailView() {
+    //     const lecturer = this.state.lecturer;
+    //     return (
+    //         <DetailForm lecturer={lecturer}/>
+    //         );        
+    // }
 
     render() {
         // const {isLoading, isEditing, lecturer} = this.state;
         // if(isLoading) {
         //    return(<h2>Loading...</h2>)
-       return this.renderDetailView()
+        const lecturer = this.state.lecturer;
+        return(
+            <div className="form-group">
+                    <form>
+                        <label>Name</label>
+                        <input type="text" className="form-control"  value={lecturer[0].name || ''} name="code" />
+                        <label>Occupation</label>
+                        <input type="text" className="form-control"  value={lecturer[0].occupation || ''} name="code"  />
+                        <label>School</label>
+                        <input type="text" className="form-control"  value={lecturer[0].school || ''} name="code"  />
+                        <label>Faculty</label>
+                        <input type="text" className="form-control"  value={lecturer[0].faculty || ''} name="code" />
+                        <label>Phone</label>
+                        <input type="text" className="form-control"  value={lecturer[0].phone || ''} name="code"  />
+                        <label>E-mail</label>
+                        <input type="text" className="form-control"  value={lecturer[0].email || ''} name="code" />
+                    </form>
+                </div>
+        );
+    //    return this.renderDetailView()
     }
 }
 
--- src/Lecturer/LecturersView.js
+++ src/Lecturer/LecturersView.js
@@ -3,25 +3,51 @@ import {Link} from 'react-router-dom';
 import LecturerDetailView from "./LecturerDetailView";
 import LecturerCard from "./LecturerCard";
 import avatar from '../images/img_avatar2.png';
-import {lecturer} from "../lecturerdata";
+//import {lecturer} from "../lecturerdata";
+import axios from 'axios';
+
+import {fetchLecturers, fetchLecturersByID} from '../api/lecturer';
 
 export default class LecturersView extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            lecturers: lecturer
+            lecturers: []
         };
 
+        // this.handleLecturerData = this.handleLecturerData.bind(this);
 
     }
     componentWillMount() {
-        this.setState({lecturer});
+        fetchLecturers()
+        .then(
+            data => this.setState({lecturers: data}))
+        .catch(function (error) {
+            if(error.response) {
+                console.log("There is an error in the lecturer data", error)
+            }
+        } )    
+
+
+        fetchLecturersByID(this.state.lecturers)
+        .then(
+            data1 => this.setState({lecturers: data1.id}))
+        .catch(function (error) {
+            console.log("There is an error in the lecuturer in the lecturer id data", error)
+        })
 
     }
 
-    loadLecturers() {
+    // handleLecturerData(data) {
+    //     let lecturerdata = [];
+    //     console.log('got lecturer from api', data);
+    //     for(let i=0; i<5; i++){
+    //         lecturerdata[i] = {name: data[i].name, occupation: data[i].occupation, school: data[i].school, faculty: data[i].faculty, phone: data[i].phone, email: data[i].email}
+    //     } 
+    //     this.setState({lecturers})
 
-    }
+    // }
+
     render() {
         return (
             <div className={'container-fluid'} >
