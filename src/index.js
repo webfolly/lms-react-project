9d2e82278aece6c8e16abb8dffaf81db0e38914c
idea-PC\jim <jianghua.hui@uq.net.au>
2018-02-13 20:44:24 +1000

    index.js updated

--- src/index.js
+++ src/index.js
@@ -4,7 +4,12 @@ import {
 	BrowserRouter as Router
 } from 'react-router-dom';
 import './styles';
+import axios from 'axios';
+//import lecturer from '../lecturer/api';
 import App from './App';
+
+
+axios.default.baseURL = 'https://lmswebapidemo.azurewebsites.net/api';
 
 ReactDOM.render(
 	<Router>
