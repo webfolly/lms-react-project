61a9de91a9f49906703544e63407c8968e117c10
idea-PC\jim <jianghua.hui@uq.net.au>
2018-02-14 00:09:11 +1000

    lecturer.js lecturer fetch, create, delete, update updated

--- src/api/lecturer.js
+++ src/api/lecturer.js
@@ -2,11 +2,23 @@ import axios from 'axios';
 //import index from 'index';
 
 export function fetchLecturers() {
-    return axios.get('/lecturer')
-    .then(response => response.data )
+    return axios.get(axios.default.baseURL + '/lecturer')
+    //.then(response => response.data )
 }
 
 export function  fetchLecturersByID(id) {
-    return axios.get('/lecturer/1')
-    .then(response => response.data.id )    
+    return axios.get(axios.default.baseURL +`/lecturer/${id}`)
+    //.then(response => response.data.id )    
+}
+
+export function createLecturer() {
+    return axios.post(axios.default.baseURL +'/lecturer');
+}
+
+export function deleteLecturerByID(id) {
+    return axios.delete(axios.default.baseURL + `/lecturer/${id}`);
+}
+
+export function updateLecturerByID(id,lecturer) {
+    return axios.put(axios.default.baseURL + `/lecturer/${id}`,lecturer)
 }
