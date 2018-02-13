fb460ff9a867e6eb0e61187d6e6b81b8d8456d4d
idea-PC\jim <jianghua.hui@uq.net.au>
2018-02-14 00:08:24 +1000

    DetailForm.js structure updated, componentwillmount, handleChange, render updated

--- src/Lecturer/DetailForm.js
+++ src/Lecturer/DetailForm.js
@@ -4,45 +4,81 @@ import {
     Route,
     Link
 } from 'react-router-dom';
-import { lecturer } from '../lecturerdata';
-import axios from 'axios';
+//import { lecturer } from '../lecturerdata';
+//import axios from 'axios';
 
 export default class DetailForm extends Component {
     
     constructor(props){
         super(props);
-        this.state = {
-            lecturer: lecturer
-        };
+        this.handleChange = this.handleChange.bind(this);
     }
 
-    renderDetailView() {
-            const lecturerlist = this.state;
-            console.log(lecturer);
-            return (
-                <div className="form-group">
-                    <form>
-                        <label>Name</label>
-                        <input type="text" className="form-control"  value={lecturer[0].name || ''} name="code" />
-                        <label>Occupation</label>
-                        <input type="text" className="form-control"  value={lecturer[0].occupation || ''} name="code"  />
-                        <label>School</label>
-                        <input type="text" className="form-control"  value={lecturer[0].school || ''} name="code"  />
-                        <label>Faculty</label>
-                        <input type="text" className="form-control"  value={lecturer[0].faculty || ''} name="code" />
-                        <label>Phone</label>
-                        <input type="text" className="form-control"  value={lecturer[0].phone || ''} name="code"  />
-                        <label>E-mail</label>
-                        <input type="text" className="form-control"  value={lecturer[0].email || ''} name="code" />
-                    </form>
-                </div>
-                );        
+    componentWillMount() {
+        this.state = this.props.object;
+        let current = ('view' === this.props.action);
+        console.log(this.props.object);
+
+        this.setState({isEditing: false});
+    }
+
+    handleChange(event) {
+        let {name, value} = event.target;
+        console.log({name});
+        if (this.state.isEditing) {
+            this.setState({lecturer: {...this.state.lecturer, [name]: value}});
+        } else {
+            return;
         }
+
+    }
+
+    // renderDetailView() {
+    //         const lecturerlist = this.state;
+    //         console.log(lecturer);
+    //         return (
+    //             <div className="form-group">
+    //                 <form>
+    //                     <label>Name</label>
+    //                     <input type="text" className="form-control"  value={lecturer[0].name || ''} name="code" />
+    //                     <label>Occupation</label>
+    //                     <input type="text" className="form-control"  value={lecturer[0].occupation || ''} name="code"  />
+    //                     <label>School</label>
+    //                     <input type="text" className="form-control"  value={lecturer[0].school || ''} name="code"  />
+    //                     <label>Faculty</label>
+    //                     <input type="text" className="form-control"  value={lecturer[0].faculty || ''} name="code" />
+    //                     <label>Phone</label>
+    //                     <input type="text" className="form-control"  value={lecturer[0].phone || ''} name="code"  />
+    //                     <label>E-mail</label>
+    //                     <input type="text" className="form-control"  value={lecturer[0].email || ''} name="code" />
+    //                 </form>
+    //             </div>
+    //             );        
+    //     }
 
     render() {
         // const {isLoading, isEditing, lecturer} = this.state;
         // if(isLoading) {
         //    return(<h2>Loading...</h2>)
-       return this.renderDetailView()
+       //return this.renderDetailView()
+        return(
+            <div className="form-group">
+                <form>
+                    <label>Name</label>
+                    <input type="text" className="form-control"  value={this.state.lecturer.name || ''} name="code" />
+                    <label>Occupation</label>
+                    <input type="text" className="form-control"  value={this.state.lecturer.occupation || ''} name="code"  />
+                    <label>School</label>
+                    <input type="text" className="form-control"  value={this.state.lecturer.school || ''} name="code"  />
+                    <label>Faculty</label>
+                    <input type="text" className="form-control"  value={this.state.lecturer.faculty || ''} name="code" />
+                    <label>Phone</label>
+                    <input type="text" className="form-control"  value={this.state.lecturer.phone || ''} name="code"  />
+                    <label>E-mail</label>
+                    <input type="text" className="form-control"  value={this.state.lecturer.email || ''} name="code" />
+                </form>
+                
+            </div>
+       );
     }
 }
