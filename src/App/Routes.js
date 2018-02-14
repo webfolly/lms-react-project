ecd5aabcadae763dc98bfe2a203c8b6c44653bd4
idea-PC\jim <jianghua.hui@uq.net.au>
2018-02-14 19:05:15 +1000

    Route.js updated

--- src/App/Routes.js
+++ src/App/Routes.js
@@ -18,7 +18,7 @@ export default () => ((
 
         <Route exact path="/students" component={StudentsView}/>
         <Route exact path="/students/detail/:action/:id" component={StudentDetailView}/>
-        <Route exact path="/lecturers/detail/:id" component={LecturerDetailView} />
+        <Route exact path="/lecturers/detail/:action/:id" component={LecturerDetailView} />
 
         <Route exact path="/lecturers" component={LecturersView}/>
         <Route exact path="/signin" component={() => <p>signin</p>}/>
