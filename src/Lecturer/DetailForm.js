33e7ecb579a89dad5a45ba23d17faf8fdbf9e240
idea-PC\jim <jianghua.hui@uq.net.au>
2018-02-14 19:04:59 +1000

    DetailForm.js updated

--- src/Lecturer/DetailForm.js
+++ src/Lecturer/DetailForm.js
@@ -6,17 +6,20 @@ import {
 } from 'react-router-dom';
 //import { lecturer } from '../lecturerdata';
 //import axios from 'axios';
-
+import LecturerDetailView from './LecturerDetailView';
 export default class DetailForm extends Component {
     
     constructor(props){
         super(props);
         this.handleChange = this.handleChange.bind(this);
+        this.handleEdit = this.handleEdit.bind(this);
+        this.handleSave = this.handleSave.bind(this)
+        this.state = {isEditing:false};
     }
 
     componentWillMount() {
         this.state = this.props.object;
-        let current = ('view' === this.props.action);
+        let object = ('view' === this.props.action);
         console.log(this.props.object);
 
         this.setState({isEditing: false});
@@ -27,10 +30,19 @@ export default class DetailForm extends Component {
         console.log({name});
         if (this.state.isEditing) {
             this.setState({lecturer: {...this.state.lecturer, [name]: value}});
+            console.log(this.lecturer)
         } else {
             return;
         }
 
+    }
+
+    handleEdit() {
+        this.setState({isEditing: true});
+    }
+
+    handleSave() {
+        this.setState({isEditing: false});
     }
 
     // renderDetailView() {
@@ -56,28 +68,57 @@ export default class DetailForm extends Component {
     //             );        
     //     }
 
+    EditButton(props) {
+        return(
+            <button onClick={props.onClick}>
+                Edit
+            </button>
+        )
+    }
+
+    SaveButton(props) {
+        return(
+            <button onClick={props.onClick}>
+                Save
+            </button>
+        )
+    }
+
+
     render() {
         // const {isLoading, isEditing, lecturer} = this.state;
         // if(isLoading) {
         //    return(<h2>Loading...</h2>)
        //return this.renderDetailView()
+       const lecturers = this.props.lecturer;
+       const currentEditing = this.state.isEditing;
+       let button = null;
+       
+    //    if(currentEditing) {
+    //        button = <EditButton onClick={this.handleEdit} />;
+    //    } else {
+    //     button = <SaveButton onClick={this.handleSave} />;
+    //    }
+       console.log( this.props.lecturer );
         return(
+           
             <div className="form-group">
                 <form>
                     <label>Name</label>
-                    <input type="text" className="form-control"  value={this.state.lecturer.name || ''} name="code" />
+                    <input type="text" className="form-control"   onChange={this.handleChange} value={lecturers.name || ''} readOnly={!this.state.isEditing} />
                     <label>Occupation</label>
-                    <input type="text" className="form-control"  value={this.state.lecturer.occupation || ''} name="code"  />
+                    <input type="text" className="form-control"  onChange={this.handleChange} value={lecturers.occupation || ''}  readOnly={!this.state.isEditing}  />
                     <label>School</label>
-                    <input type="text" className="form-control"  value={this.state.lecturer.school || ''} name="code"  />
+                    <input type="text" className="form-control"  onChange={this.handleChange} value={lecturers.school || ''}  readOnly={!this.state.isEditing}  />
                     <label>Faculty</label>
-                    <input type="text" className="form-control"  value={this.state.lecturer.faculty || ''} name="code" />
+                    <input type="text" className="form-control"  onChange={this.handleChange} value={lecturers.faculty || ''}  readOnly={!this.state.isEditing} />
                     <label>Phone</label>
-                    <input type="text" className="form-control"  value={this.state.lecturer.phone || ''} name="code"  />
+                    <input type="text" className="form-control" onChange={this.handleChange}  value={lecturers.phone || ''}  readOnly={!this.state.isEditing}  />
                     <label>E-mail</label>
-                    <input type="text" className="form-control"  value={this.state.lecturer.email || ''} name="code" />
+                    <input type="text" className="form-control" onChange={this.handleChange} value={lecturers.email || ''}  readOnly={!this.state.isEditing} />
                 </form>
-                
+            {button}
+              
             </div>
        );
     }
