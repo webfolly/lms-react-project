b52cdc2a0abe450bd177a3f882e2868bb308eae7
idea-PC\jim <jianghua.hui@uq.net.au>
2018-02-14 19:18:33 +1000

    lecturer.js updated

--- src/api/lecturer.js
+++ src/api/lecturer.js
@@ -2,23 +2,23 @@ import axios from 'axios';
 //import index from 'index';
 
 export function fetchLecturers() {
-    return axios.get(axios.default.baseURL + '/lecturer')
+    return axios.get('/lecturer')
     //.then(response => response.data )
 }
 
 export function  fetchLecturersByID(id) {
-    return axios.get(axios.default.baseURL +`/lecturer/${id}`)
+    return axios.get(`/lecturer/${id}`)
     //.then(response => response.data.id )    
 }
 
 export function createLecturer() {
-    return axios.post(axios.default.baseURL +'/lecturer');
+    return axios.post('/lecturer');
 }
 
 export function deleteLecturerByID(id) {
-    return axios.delete(axios.default.baseURL + `/lecturer/${id}`);
+    return axios.delete(`/lecturer/${id}`);
 }
 
 export function updateLecturerByID(id,lecturer) {
-    return axios.put(axios.default.baseURL + `/lecturer/${id}`,lecturer)
+    return axios.put(`/lecturer/${id}`,lecturer)
 }
